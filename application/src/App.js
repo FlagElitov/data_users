22f857cf73e6cd4d7d1c96e54536853e0e33d2c5
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2020-10-17 09:27:58 +0300

    add Apollo client

--- application/src/App.js
+++ application/src/App.js
@@ -1,45 +1,18 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  AppBar,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import "./App.scss";
+import MenuBar from "./components/MenuBar";
+import UserList from "./components/UserList";
 
 const App = () => {
-  const classes = useStyles();
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          Data User
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <MenuBar />
+      <div className="container">
+        <div className="userlist">
+          <UserList />
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ application/src/components/UserList.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Checkbox from "@material-ui/core/Checkbox";
+import Avatar from "@material-ui/core/Avatar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 1170,
+    marginTop: 20,
+    backgroundColor: theme.palette.background.paper,
+  },
+  userList: {
+    marginTop: 10,
+  },
+}));
+
+export default function CheckboxListSecondary() {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState([1]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <List dense className={classes.root}>
+      {[0, 1, 2, 3].map((value) => {
+        const labelId = `checkbox-list-secondary-label-${value}`;
+        return (
+          <ListItem className={classes.userList} key={value} button>
+            <ListItemAvatar>
+              <Avatar
+                alt={`Avatar n°${value + 1}`}
+                src={`/static/images/avatar/${value + 1}.jpg`}
+              />
+            </ListItemAvatar>
+            <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+            <ListItemSecondaryAction>
+              <Checkbox
+                edge="end"
+                onChange={handleToggle(value)}
+                checked={checked.indexOf(value) !== -1}
+                inputProps={{ "aria-labelledby": labelId }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
+
+// import React from "react";
+// import { gql } from "apollo-boost";
+// import { graphql } from "apollo-boost";
+
+// const getUsersQuery = gql`
+//   query users {
+//     users {
+//       id
+//       name
+//       email
+//     }
+//   }
+// `;
+
+// const UserList = (props) => {
+//   return <h1>dfs</h1>;
+// };
+
+// export default UserList;
