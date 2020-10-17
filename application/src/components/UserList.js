b9c88fb17fca5d91530affca9803bdf239aae642
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2020-10-17 20:31:15 +0300

    update application .

--- application/src/components/UserList.js
+++ application/src/components/UserList.js
@@ -5,24 +5,51 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Checkbox from "@material-ui/core/Checkbox";
 import Avatar from "@material-ui/core/Avatar";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import EditIcon from "@material-ui/icons/Edit";
+
+import { useQuery, gql } from "@apollo/client";
+import { Button, Fab } from "@material-ui/core";
+import Loader from "../asses/loader";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     maxWidth: 1170,
-    marginTop: 20,
+    marginTop: 10,
     backgroundColor: theme.palette.background.paper,
+  },
+  button: {
+    margin: theme.spacing(1),
   },
   userList: {
     marginTop: 10,
   },
+  icon: {
+    fontSize: 40,
+  },
 }));
 
-export default function CheckboxListSecondary() {
+const GET_USERS_QUERY = gql`
+  query users {
+    users {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const UserList = () => {
+  const { loading, error, data } = useQuery(GET_USERS_QUERY);
+  const [checked, setChecked] = React.useState([]);
   const classes = useStyles();
-  const [checked, setChecked] = React.useState([1]);
+  console.log(data);
+  if (loading) return <div className='loader'> <Loader /></div>;
+  if (error) return <p>Error :(</p>;
 
   const handleToggle = (value) => () => {
     const currentIndex = checked.indexOf(value);
@@ -39,6 +66,21 @@ export default function CheckboxListSecondary() {
 
   return (
     <List dense className={classes.root}>
+      <div className="center">
+        <Fab
+          size="medium"
+          color="secondary"
+          aria-label="add"
+          className={classes.margin}
+        >
+          <AddIcon />
+        </Fab>
+        {/* <IconButton edge="end" aria-label="delete">
+          <Icon className={classes.icon} color="secondary">
+            add_circle
+          </Icon>
+        </IconButton> */}
+      </div>
       {[0, 1, 2, 3].map((value) => {
         const labelId = `checkbox-list-secondary-label-${value}`;
         return (
@@ -51,19 +93,21 @@ export default function CheckboxListSecondary() {
             </ListItemAvatar>
             <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
             <ListItemSecondaryAction>
-              <Checkbox
-                edge="end"
-                onChange={handleToggle(value)}
-                checked={checked.indexOf(value) !== -1}
-                inputProps={{ "aria-labelledby": labelId }}
-              />
+              <Fab color="secondary" size="small" aria-label="edit">
+                <EditIcon />
+              </Fab>
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
             </ListItemSecondaryAction>
           </ListItem>
         );
       })}
     </List>
   );
-}
+};
+
+export default UserList;
 
 // import React from "react";
 // import { gql } from "apollo-boost";
--- application/src/index.js
+++ application/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
